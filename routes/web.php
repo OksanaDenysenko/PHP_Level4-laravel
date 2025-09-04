f79d1e0a85fb68949d07bb227b518b24b2328ae4
OksanaDenysenko <oksana.krivopisha@gmail.com>
2025-09-04 07:35:05 +0000

    added routes for PersonController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/people', [PersonController::class,'index'])->name('people.index');
+Route::get('/people/create', [PersonController::class,'create'])->name('people.create');
+Route::post('/people/store', [PersonController::class,'store'])->name('people.store');
+Route::put('/people/{person}', [PersonController::class,'update'])->name('people.update');
+Route::delete('/people/{person}', [PersonController::class,'destroy'])->name('people.destroy');
